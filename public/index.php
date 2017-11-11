583e8f7d0cb95cfba5916625d30f648740a72afb
Pablo <pablo@getrxtrax.com>
2017-11-11 02:15:01 -0700

    added header to overcome CORS crap

--- public/index.php
+++ public/index.php
@@ -3,6 +3,11 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PATCH');
+header('Access-Control-Allow-Headers: content-type');
+header("Content-Type: text/plain application/x-www-form-urlencoded");
+
 use pantry\App;
 
 $app = new App();
